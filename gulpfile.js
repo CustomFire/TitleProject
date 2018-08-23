e0458f5aa74b03329641a4a05e981f4a291ad3cf
CustomFire <bohdan.korolev@gmail.com>
2018-08-23 17:46:02 +0300

    home adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,18 @@ gulp.task('sass', function() { // Создаем таск "sass"
     .pipe(gulp.dest('css')) // Выгружаем результата в папку css
   });
 
-  gulp.task('browser-sync', function() { // Создаем таск browser-sync
-    browserSync({ // Выполняем browserSync
-      server: { // Определяем параметры сервера
-          baseDir: 'app' // Директория для сервера - app
-      },
-      notify: false // Отключаем уведомления
-    });
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+  browserSync({ // Выполняем browserSync
+    server: { // Определяем параметры сервера
+        baseDir: './' // Директория для сервера - app
+    },
+    notify: false // Отключаем уведомления
   });
+});
 
-gulp.task('watch', function() {
+gulp.task('watch', ['browser-sync', 'sass'], function() {
   gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
-  gulp.watch('pages/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+  gulp.watch('pages/**/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
 });
 
 gulp.task('default', ['watch']);
